ebc5cddc9d40a990892ef034ec77f6cd3f744329
SHmohamed229 <SHmohamed229@gmail.com>
2023-10-16 23:14:12 -0700

    all

---
+++ 0x14-mysql/2-db_setup.sql
@@ -0,0 +1,17 @@
+-- this script for Creates the primary database with a populated table
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
+
+CREATE TABLE IF NOT EXISTS nexus6(
+	id INT,
+	name VARCHAR(256)
+);
+
+INSERT INTO nexus6 VALUES
+	(1, 'Mohamed'),
+	(2, 'Marwaa')
+;
+
+USE mysql;
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
